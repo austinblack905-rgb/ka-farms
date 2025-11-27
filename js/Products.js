228f4126c99fe56c044e743caafe7a32322b3ba8
austinblack905-rgb <austinblack905@gmail.com>
2025-11-26 23:49:52 -0500

    Add files via upload

---
+++ js/Products.js
@@ -0,0 +1,45 @@
+// Products
+window.PRODUCTS = {
+  "eggs": {
+    "title": "Farm-Fresh Eggs",
+    "price": 5,
+    "thumb": "Eggs.jpg",
+    "inventory": 10
+  },
+  "scrubber": {
+    "title": "Egg Scrubber",
+    "price": 6,
+    "thumb": "Egg Scrubber.jpg",
+    "inventory": 3
+  },
+  "vanilla": {
+    "title": "Vanilla Extract",
+    "price": 5,
+    "thumb": "Vanilla.jpg",
+    "inventory": 68
+  },
+  "crinkle": {
+    "title": "Crinkle Cookies",
+    "price": 10,
+    "thumb": "Crinkle Cookie.jpg",
+    "inventory": "By Order"
+  },
+  "butterCake": {
+    "title": "Ooey Gooey Butter Cake",
+    "price": 20,
+    "thumb": "Ooey Gooey.png",
+    "inventory": "By Order"
+  },
+  "chocChip": {
+    "title": "Chocolate Chip Cookies",
+    "price": 12,
+    "thumb": "Chocolate Chip.jpg",
+    "inventory": "By Order"
+  },
+  "cupCookie": {
+    "title": "Peanut Butter Cup Cookies",
+    "price": 14,
+    "thumb": "CupCookies.jpg",
+    "inventory": "By Order"
+  }
+};
